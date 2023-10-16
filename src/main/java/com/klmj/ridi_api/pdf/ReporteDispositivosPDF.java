7038c21ee062fa0d4c40c98d9a2ed1e57efc2ad2
Maguchi19 <maguchi109@icloud.com>
2023-10-15 21:37:38 -0600

    Avance de pdf

---
+++ src/main/java/com/klmj/ridi_api/pdf/ReporteDispositivosPDF.java
@@ -0,0 +1,27 @@
+package com.klmj.ridi_api.pdf;
+
+import com.klmj.ridi_api.persistence.entity.Dispositivo;
+import com.lowagie.text.pdf.PdfPTable;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+import org.springframework.stereotype.Component;
+import org.springframework.web.servlet.view.document.AbstractPdfView;
+
+import java.util.List;
+import java.util.Map;
+
+@Component("")
+public class ReporteDispositivosPDF extends AbstractPdfView {
+
+    @Override
+    protected void buildPdfDocument(Map<String, Object> model, com.lowagie.text.Document document, com.lowagie.text.pdf.PdfWriter writer, HttpServletRequest request, HttpServletResponse response) throws Exception {
+        @SuppressWarnings("unchecked")
+        List<Dispositivo> dispositivoList = (List<Dispositivo>) model.get("");
+        int columnas = 1;
+        PdfPTable tablaReportes = new PdfPTable(columnas);
+
+        dispositivoList.forEach(dispositivo -> {
+            tablaReportes.addCell(dispositivo.getId());
+        });
+    }
+}
