dc6db954dee95f9296e453c9d70c6e2be041bf7e
Maguchi19 <maguchi109@icloud.com>
2023-10-30 14:03:33 -0600

    Borre controladores grupo, EstatusIncidencia. alv Kevin como chingas

--- src/main/java/com/klmj/ridi_api/persistence/repository/GrupoRepository.java
+++
@@ -1,9 +0,0 @@
-package com.klmj.ridi_api.persistence.repository;
-
-import com.klmj.ridi_api.persistence.entity.Grupo;
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
-
-@Repository
-public interface GrupoRepository extends JpaRepository<Grupo, Enum> {
-}
