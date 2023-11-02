abfdfd56ca84f51dec10a756e66925c8a572ee12
KevinCyndaquil <aleplantsvsz@gmail.com>
2023-11-01 22:05:03 -0600

    Ya quedaron los controladores y servicios base, agregue una portada y un uml

--- src/main/java/com/klmj/ridi_api/persistence/repository/management/PerifericoRepository.java
+++ src/main/java/com/klmj/ridi_api/persistence/repository/management/PerifericoRepository.java
@@ -1,7 +1,6 @@
 package com.klmj.ridi_api.persistence.repository.management;
 
 import com.klmj.ridi_api.persistence.entity.management.Periferico;
-import com.klmj.ridi_api.persistence.entity.management.TipoPeriferico;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
